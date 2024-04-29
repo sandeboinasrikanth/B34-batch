a08f0640dfb64fbf5451c2f9c06ac0ecdac071e8
sandeboina srikanth <sandeboinasrikanth@gmail.com>
2024-04-29 20:56:43 +0530

    dom-problems-added

--- guvi-Lavish-classes/day2.js
+++ guvi-Lavish-classes/day2.js
@@ -85,4 +85,57 @@ let disliked=()=>{
 }
 //-------------------------------------
 
+/*
+What is diff b/w setTimeOut vs setInterval fun ?
+=> These are two function that comes under web Api.
+ex: you want for Restaurant
+   cup of tea  => 1hr
+   food = 30mins 
+   juice, starter => ready food
+
+   web Api
+
+   setTimeout => runs your code only once after the timeout is completed.
+
+   setInterval => we runs code / function repeatedly for each interval
+
+ */
+
+// 1s = 1000 milliseconds
+setTimeout(()=>{
+    console.log("I will Excecute After 5000 MilliSeconds")
+},5000)
+
+//another way
+function displayData(){
+    console.log("I will Excecute after 3000ms")
+}
+
+// let setTimer = setTimeout(displayData, 3000);
+
+setTimeout(()=>{
+    clearTimeout(setTimer)
+},4000)
+
+//setTimeout => web Api function that excecute your code only for once delay time is over.
+
+
+//setInterval
+
+let i=0;
+let fruits = ["papaya","mangoes","oranges","pineapple","grapes"];
+
+function displayFruit(){
+    console.log(fruits[i]);
+    i++;
+}
+
+let setTimers = setInterval(displayFruit, 1000);
+
+setInterval(()=>{
+    // clearInterval(setTimers)
+    clearTimeout(setTimers)
+}, 2000)
+
+
 
---
+++ guvi-Lavish-classes/day3.js
@@ -0,0 +1,114 @@
+/*  
+// Recap of Dom Manupulation
+Dom =>
+what is node in Dom ? => node is nothing but element, collection of elements, 
+
+diff b/w window and document
+window => 
+document => 
+
+Recap of DOM ManipulationDOM=>NodeWindow and DocumentDOm Selector?
+1.getElementById("")=>return=>id value
+2.getElementsByClassName()=>array of element with the given class name
+3.getElementByTag()=>html tagnamereturn an array of elements with given tag name
+4.querySelectorall()=>this method used to find all html element with specified selector
+                    (id,class name,value,type,attribute)collection of element
+5.querySelector()=>select single elements
+
+ */
+
+/*
+problem: 
+1.User => create a input box with parameter like 
+id , class , attribute
+
+2.take a string as a input from user.
+
+3.Calculate the length of string.
+
+implement the same with the help of dom manupulation.
+ */
+
+document.querySelector("#btn").addEventListener("click", function(){
+    let wordLength = document.querySelector("#str").value.length;
+    console.log(wordLength);
+
+    let output = document.querySelector(".output").innerHTML = wordLength;
+    
+})
+
+/*
+Guessing the no.
+
+*/
+function guessNumber(){
+    let numberGuessing = document.querySelector("#guess-no");
+
+    let count = numberGuessing.value.length;
+
+    if(count <=5){
+        alert("The number you guess is too low")
+    }else if(count === 5){
+        alert("The Number you guessed is equal ")
+    }else if(count >=5){
+        alert("The number you guessed is too high")
+    }
+
+    numberGuessing.innerHTML = count;
+}
+
+/* 
+don't use single element in index.html
+
+create a dropdown box where user should add a new country name everytime by click on add button
+
+*/
+let countriesInfo = [
+    {
+      name: "India",
+      population: " 1,394,975,829",
+      region: "Asia",
+      flag: "https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png",
+      capital: "New delhi",
+    },
+    {
+      name: "Germany",
+      population: "81,770,900",
+      region: "Europe",
+      flag: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png",
+      capital: "Berlin",
+    },
+    {
+      name: "United states",
+      population: "323,947,000",
+      region: "Americas",
+      flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/640px-Flag_of_the_United_States.svg.png",
+      capital: "Washington D.C",
+    },
+    {
+      name: "Brazil",
+      population: "206,135,893",
+      region: "Americas",
+      flag: "https://upload.wikimedia.org/wikipedia/commons/0/01/Brazil_flag_300.png",
+      capital: "Brasilia",
+    },
+    {
+      name: "Iceland",
+      population: "334,300",
+      region: "Europe",
+      flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/640px-Flag_of_Iceland.svg.png",
+      capital: "Reykjavik",
+    },
+  ];
+
+let countryInfoContainer = document.createElement("div");
+countryInfoContainer.className = "container";
+
+console.log(countryInfoContainer);
+
+let countryName = document.createElement("h2");
+countryName.innerHTML= "India";
+
+let population = document.querySelector("");
+
+
